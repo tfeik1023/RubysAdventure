bdd549637589c47e4a6b6004f91a995ed543653f
Tyler Feikert <you@example.com>
2022-04-06 22:03:52 -0400

    Challenge commit

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -35,5 +35,13 @@ public class Projectile : MonoBehaviour
         }
 
         Destroy(gameObject);
+
+        HardEnemyController h =other.collider.GetComponent<HardEnemyController>();
+        if (h !=null)
+        {
+            h.Fix();
+        }
+
+        Destroy(gameObject);
     }
 }
